30120c1a8e248a049873418b734b133e5e383ba5
Marko <ilya1501@gmail.com>
2022-10-27 13:50:20 +0300

    Read file

---
+++ Perevod/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Perevod
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Profession { get; set; }
+        public int Age { get; set; }
+        public Address Address { get; set; }
+
+       
+    }
+}
