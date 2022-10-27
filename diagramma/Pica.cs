e9f65d8e40588901bfa262f39229b9ac7a042f68
Marko <ilya1501@gmail.com>
2022-10-27 13:13:17 +0300

    technical description

---
+++ diagramma/Pica.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace diagramma
+{
+    public class Pica 
+    {
+        public class Pica : Telephone
+        {
+            public Pica()
+            {
+            }
+
+            public override bool Equals(object obj)
+            {
+                return obj is Pica pica &&
+                       bell == pica.bell &&
+                       CalculatingTheOperator == pica.CalculatingTheOperator;
+            }
+
+            public override int GetHashCode()
+            {
+                return HashCode.Combine(bell, CalculatingTheOperator);
+            }
+        }
+
+        private readonly string _CPU;
+
+        public string cpu;
+
+        
+       public int NumbeOfSockets { get; set; }
+
+        public string Model { get; set; }
+
+
+
+
+
+    }
+}
