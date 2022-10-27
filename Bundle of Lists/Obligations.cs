37d69df1960495a00c13d14ea4f16fc9c1cde7ec
Marko <ilya1501@gmail.com>
2022-10-27 13:22:40 +0300

    test list

---
+++ Bundle of Lists/Obligations.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bundle_of_Lists
+{
+   public class Obligations<T>
+    {
+        public T Item { get; set; }
+        public Obligations(T item)
+        {
+            Item = item;
+        }
+    }
+}
---
+++ Bundle of Lists/Postman.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bundle_of_Lists
+{
+   public class Postman<T>
+    {
+        Postman<T> StartWd;
+        Postman<T> Ending;
+
+
+        public void Add(T item)
+        {
+
+        }
+    }
+
+
+}
+
