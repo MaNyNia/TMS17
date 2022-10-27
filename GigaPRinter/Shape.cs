7ab20fade728e24a8e227e40fa002f78ba92836b
Marko <ilya1501@gmail.com>
2022-10-27 13:02:47 +0300

    ShapePrint?

---
+++ GigaPRinter/Shape.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GigaPRinter
+{
+    class Shape
+    {
+        private static void ConsolePyramid(int rowCount)
+        {
+            int consoleCenter = Console.WindowWidth / 2;
+            for (int row = 0; row < rowCount; row++)
+                Console.WriteLine(
+                    "".PadLeft(1 + row * 2, '^').PadLeft(consoleCenter + row, ' '));
+        }
+
+        private static void ConsolePyramidBunch(int pyrCount)
+        {
+            for (int pyrIndex = 1; pyrIndex <= pyrCount; pyrIndex++)
+                ConsolePyramid(pyrIndex);
+        }
+    }
+}
