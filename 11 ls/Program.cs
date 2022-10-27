ffa088b3925d172a5116724a68f8df938c0a76bb
Marko <ilya1501@gmail.com>
2022-10-27 13:28:34 +0300

    Table v1.0

---
+++ 11 ls/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace _11_ls
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Table<int, string, int> table = new Table<int, string, int>();
+            Type type = table.GetType();
+
+            table.AddRow(1, "Ivan", 2);
+            table.AddRow(2, "Jeka", 4);
+            table.AddRow(3, "Vova", 5);
+            table.AddRow(4, "Kate", 3);
+
+            Line line = new Line();
+
+            line.PrintLine();
+            line.PrintRow("T № "," U(name) ", "Z(points) ");
+            line.PrintLine();
+            line.PrintRow("", "", "");
+            line.PrintRow("", "", "");
+            line.PrintLine();
+            Console.ReadLine();
+
+           
+
+        }
+
+        
+    }
+
+    
+}
