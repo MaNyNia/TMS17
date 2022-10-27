70639c402a822c1094a1b8a1057ef7a24cb3c5e6
Marko <ilya1501@gmail.com>
2022-10-27 13:45:56 +0300

    table v2.0

---
+++ Wk_List/Table.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wk_List
+{
+   public class Table<T, U, Z>
+    {
+        public static List<T> St { get; set; }
+        public static List<U> Su { get; set; }
+        public static List<Z> Sz { get; set; }
+        public Table()
+        {
+            St = new List<T>();
+            Su = new List<U>();
+            Sz = new List<Z>();
+        }
+
+        public void AddRow(T st, U su, Z sz)
+        {
+            St.Add(st);
+            Su.Add(su);
+            Sz.Add(sz);
+        }
+
+        public void Print()
+        {
+
+        }
+    }
+    }
