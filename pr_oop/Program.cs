48dcd5841ead2ab76e1c1f1fe281a548831f4eef
Marko <ilya1501@gmail.com>
2022-10-27 13:30:13 +0300

    what?

---
+++ pr_oop/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace pr_oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var courses = new TeachMeSkillsCourses(12);
+        }
+        
+    }
+
+    
+}
