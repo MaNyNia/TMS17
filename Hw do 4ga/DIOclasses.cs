53be323269f8f6fd428fd800eee52a2f9c991ee5
Marko <ilya1501@gmail.com>
2022-11-03 13:50:11 +0300

    Peredacha

---
+++ Hw do 4ga/DIOclasses.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hw_do_4ga
+{
+    public interface IDecerializer
+    {
+        public List<Camera> Decerialize(string path);
+    }
+
+}
---
+++ Hw do 4ga/Decerializer.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hw_do_4ga
+{
+
+    public class Decerializer : IDecerializer
+    {
+        private readonly IConfiguration _config;
+        public List<Camera> Decerialize(string path)
+        {
+            using (StreamReader streamReader = new StreamReader(path))
+            {
+                var json = streamReader.ReadToEnd();
+                return JsonConvert.DeserializeObject<List<Camera>>(json);
+            }
+        }
+    }
+
+}
