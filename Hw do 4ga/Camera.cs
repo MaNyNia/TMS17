007d67cdc84cffa891823759e919348b9d8ca7bc
Marko <ilya1501@gmail.com>
2022-11-03 13:46:32 +0300

    param + list

---
+++ Hw do 4ga/Camera.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hw_do_4ga
+{
+    public class Camera
+    {
+        public double Price { get; set; }
+        public string Firm { get; set; }
+        public string Model { get; set; }
+        public int Lens { get; set; }
+        public char Functions { get; set; }
+        public bool Availability { get; set; }
+       
+    }
+}
